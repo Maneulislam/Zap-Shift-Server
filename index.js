d56af1767917e2dd4bc6920365d1826e515d3c4c
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-08-24 17:57:36 +0600

    Added delete api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const dotenv = require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 
 
@@ -60,6 +60,14 @@ async function run() {
 
             const result = await parcelsCollections.insertOne(parcel);
             res.send(result);
+        })
+
+
+        app.delete('/parcels/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await parcelsCollections.deleteOne(query);
+            res.send(result)
         })
 
 
